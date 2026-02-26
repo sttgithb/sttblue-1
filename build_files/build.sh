65755f9920a9c7980eef6f8ea6b274e7603911e5
sttgithb <sttgithb@gmail.com>
2026-02-26 08:23:10 -0800

    Update build.sh
    First Flatpak install test from Flathub

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,3 +9,7 @@ for drv in qemu interface network nodedev nwfilter secret storage; do \
     systemctl enable virt${drv}d.service; \
     systemctl enable virt${drv}d{,-ro,-admin}.socket; \
     done
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install --noninteractive flathub com.brave.Browser
